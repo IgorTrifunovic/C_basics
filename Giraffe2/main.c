34ddc5745377f6428209518dd448a40dc2abc1b0
IgorTrifunovic <igor13trifunovic@gmail.com>
2022-09-13 10:13:50 +0200

    day2

--- Giraffe2/main.c
+++ Giraffe2/main.c
@@ -3,10 +3,15 @@
 
 int main()
 {
-    int age;
-    printf("Enter your age: ");
-    scanf("%d", &age);
-    printf("You are %d year old.\n");
+    // int age;
+    // printf("Enter your age: ");
+    // scanf("%d", &age);
+    // printf("You are %d year old.\n");
+
+    char name[20];
+    printf("Enter your name: ");
+    fgets(name, 20, stdin);
+    printf("Your name is %s asdf", name);
 
     return 0;
 }
